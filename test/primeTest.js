724ff93d4d95de63deedb114880e0ded1f23f284
Giuseppe DeLuca <77351244+GiupiDeLuca@users.noreply.github.com>
2022-05-10 13:57:47 -0500

    Update primeTest.js

--- test/primeTest.js
+++ test/primeTest.js
@@ -10,15 +10,15 @@ contract("Prime", async (accounts) => {
   });
 
   it("Should mint 1000 PRIME tokens for a user", async () => {
-    await prime.mint(accounts[1], 1000);
+    await prime.mint(accounts[2], 1000);
 
     const totalSupply = await prime.totalSupply();
-    const balance1 = await prime.balanceOf(accounts[1]);
+    const balance1 = await prime.balanceOf(accounts[2]);
 
     // asserts that the total supply is, at this point, 1000
     assert(totalSupply.toNumber() == 1000);
     
-    // expects balance of accounts[1] to be 1000
+    // expects balance of accounts[2] to be 1000
     expect(balance1.toNumber()).to.equal(1000);
   });
 });
